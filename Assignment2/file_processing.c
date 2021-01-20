bb95e7c8019ed0ceac2fb8a52167350bc8efde6f
Orion Junkins <junkinso@oregonstate.edu>
2021-01-20 14:36:06 -0800

    filename selection working with limited testing

--- Assignment2/file_processing.c
+++ Assignment2/file_processing.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
 #define MAX_INPUT_LENGTH 100
 #define MAX_FILENAME_LENGTH 100
 #define PREFIX "movies"
 #define SUFFIX ".csv"
-
+#define LONG_LONG_INT_MAX 9223372036854775807
 void prompt_for_name(char* filename);
+void largest_file_in_cur_dir(char* filename);
+bool filename_matches_format(char* filename);
+void smallest_file_in_cur_dir(char* filename);
 
 int user_selection(char* prompt, int min, int max) 
 {
@@ -58,6 +66,7 @@ void select_file(FILE* selected_file, int selection)
         break;
     case 2:
         // 
+        smallest_file_in_cur_dir(filename);
         break;
     case 3:
         // Specify the name of a file 
@@ -73,21 +82,62 @@ void select_file(FILE* selected_file, int selection)
     free(filename);
 }
 
-void largest_file_in_cur_dir(filename)
+void largest_file_in_cur_dir(char* filename)
 {
+    off_t largest_file_size = -1;
     DIR* currDir = opendir(".");
     struct dirent *aDir;
     struct stat dirStat;
 
     // Go through all the entries
     while((aDir = readdir(currDir)) != NULL)
-    {
-        if (strncmp(PREFIX, aDir->d_name, strlen(PREFIX)) == 0 && strcmp(str + strlen(aDir->d_name) - (1 + strlen(SUFFIX)), SUFFIX) == 0) //Extract to bool function
+    {        
+        if(filename_matches_format(aDir->d_name))
         {
-            printf("/n%s", aDir->d_name);
+            stat(aDir->d_name, &dirStat);
+            if(dirStat.st_size > largest_file_size)
+            {
+                largest_file_size = dirStat.st_size;
+                strcpy(filename, aDir->d_name);
+            }
         }
     }
 }
+
+void smallest_file_in_cur_dir(char* filename)
+{
+    off_t smallest_file_size = LONG_LONG_INT_MAX;
+    DIR* currDir = opendir(".");
+    struct dirent *aDir;
+    struct stat dirStat;
+
+    // Go through all the entries
+    while((aDir = readdir(currDir)) != NULL)
+    {        
+        if(filename_matches_format(aDir->d_name))
+        {
+            stat(aDir->d_name, &dirStat);
+            if(dirStat.st_size < smallest_file_size)
+            {
+                smallest_file_size = dirStat.st_size;
+                strcpy(filename, aDir->d_name);
+            }
+        }
+    }
+}
+
+bool filename_matches_format(char* filename)
+{
+    if (strncmp(PREFIX, filename, strlen(PREFIX)) == 0)
+    {
+        if(strcmp(filename + strlen(filename) - strlen(SUFFIX), SUFFIX) == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 void prompt_for_name(char* filename)
 {
     printf("Enter the complete file name: ");
