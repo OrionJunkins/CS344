191dfc9d0963224ca273b3b8a9d68c231e357ab4
Orion Junkins <junkinso@oregonstate.edu>
2021-01-28 19:41:37 -0800

    Command struct created

--- Assignment3/command_processing.h
+++ Assignment3/command_processing.h
@@ -2,4 +2,13 @@
 #include <stdbool.h>
 #include <string.h>
 #define COMMAND_BUFFER_SIZE 2048
+#define MAX_NUM_ARGS 512
 bool is_exit_command(char* command);
+
+typedef struct Command {
+    char command[COMMAND_BUFFER_SIZE];
+    char* arguments[MAX_NUM_ARGS];
+    char* infile;
+    char* outfile;
+    bool background;
+} Command;
