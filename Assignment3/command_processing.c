46462137c471207398d98f88215ff008b70cd325
Orion Junkins <junkinso@oregonstate.edu>
2021-01-29 20:17:10 -0800

    basic external execvp call working

--- Assignment3/command_processing.c
+++ Assignment3/command_processing.c
@@ -44,16 +44,19 @@ void execute(char* input_command) {
     /*
         Execute the given command
     */
-   Command* command = malloc(sizeof(Command)); 
+    Command* command = malloc(sizeof(Command)); 
+    command->arg_count = 0;
 
-   parse_command(input_command, command);
+    parse_command(input_command, command);
 
-   //redirect_files(command); TODO
+    if (is_builtin(command)){
+        exec_internal(command);
+    } else {
+        exec_external(command);
+    }
 
-   free(command);
+    free(command);
 }
-
-
 
 void parse_command(char* input_command, Command* command) {
     /*
@@ -67,7 +70,6 @@ void parse_command(char* input_command, Command* command) {
 
     pt = strtok_r(NULL, " ", &save_ptr);
 
-    int arg_index = 0;
     while(pt != NULL){
         if(strcmp("&",pt) == 0){
             command->background = true;
@@ -80,10 +82,52 @@ void parse_command(char* input_command, Command* command) {
         } else if(strcmp("$$",pt) == 0){
             printf(" $$ \n");
         }else{
-            strcpy(command->arguments[arg_index], pt);            
-            arg_index += 1;
+            strcpy(command->arguments[command->arg_count], pt);            
+            command->arg_count += 1;
         }
         pt = strtok_r(NULL, " ", &save_ptr); 
     }
+    for (int i = 0; i < command->arg_count; i++){
+        printf("arg %d: %s\n", i, command->arguments[i]);
+    }
 }
 
+bool is_builtin(Command* command){
+    /*
+        If the given command has a name which is included in the array of builtins, return true, else false
+    */
+    for (int i = 0; i < NUM_BUILTINS; i++){
+        if(strcmp(BUILTIN_COMMANDS[i], command->command_name) == 0){
+            return true;
+        }
+    }
+    return false;
+}
+
+void exec_internal(Command* command){
+    /* 
+        Given that a command is internally defined, execute it
+    */
+
+}
+
+void exec_external(Command* command){
+    /* 
+        Given that a command is externally defined, execute it
+    */
+    char* args[MAX_NUM_ARGS];
+    args[0] = command->command_name;
+    for (int i = 0; i < command->arg_count; i++){
+        args[i+1] = command->arguments[i]; 
+    }
+    args[command->arg_count + 1] = NULL;
+
+    
+    printf("command: %s\n", command->command_name);
+    for (int i = 0; i < command->arg_count + 2; i++){
+        printf("exec arg %d: %s\n", i, args[i]);
+    }
+
+    execvp(args[0], args);
+    printf("error");
+}
--- Assignment3/command_processing.h
+++ Assignment3/command_processing.h
@@ -2,19 +2,32 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h> 
+#include <unistd.h>
 #define COMMAND_BUFFER_SIZE 2048
 #define MAX_NUM_ARGS 512
-bool is_exit_command(char* command);
+
+#define NUM_BUILTINS 3
+const char BUILTIN_COMMANDS[NUM_BUILTINS][20] = {
+    "exit",
+    "cd",
+    "status",
+};
+
 
 typedef struct Command {
     char command_name[COMMAND_BUFFER_SIZE];
     char arguments[MAX_NUM_ARGS][COMMAND_BUFFER_SIZE];
+    int arg_count;
     char infile[COMMAND_BUFFER_SIZE];
     char outfile[COMMAND_BUFFER_SIZE];
     bool background;
     } Command; 
 
 void parse_command(char* input_command, Command* command);
+bool is_builtin(Command* command);
+bool is_exit_command(char* command);
+void exec_external(Command* command);
+void exec_internal(Command* command);
 
 
 
