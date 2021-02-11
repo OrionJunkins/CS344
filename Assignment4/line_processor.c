2c440e618c6d96922a1e13a0eaaee25000e99d3a
Orion Junkins <junkinso@oregonstate.edu>
2021-02-10 19:43:51 -0800

    first pro/com pair outlined

--- Assignment4/line_processor.c
+++ Assignment4/line_processor.c
@@ -1,17 +1,74 @@
 #include "line_processor.h"
 
-void get_input_lines(char* input_buffer) {
+void* get_input_lines(void* arg) {
     
-    ssize_t line_size;
-    size_t input_buffer_size = MAX_LINE_SIZE;
+    char tmp_buffer[MAX_LINE_SIZE];
+    while(1){
+        // Get a line to a temporary buffer
+        memset(tmp_buffer, '\0', MAX_LINE_SIZE);
+        char* getline_buffer = tmp_buffer;
+        ssize_t line_size;
+        size_t buffer_size = MAX_LINE_SIZE;
+        line_size = getline(&getline_buffer, &buffer_size, stdin);
+        // Lock the input buffer mutex. If it is already locked, wait here
+        pthread_mutex_lock(&input_buffer_mutex); 
 
-    // wait for flag that buffer has been emptied
-    // Lock
-    line_size = getline(&input_buffer, &input_buffer_size, stdin);
-    // Unlock
+        // Copy the line gotten to input_buf
+        strcpy(input_buffer, tmp_buffer);
+        
+        // Notify consumer that input_buf has data
+        pthread_cond_signal(&input_buffer_has_data); 
+
+        // Wait until consumer empties the buffer
+        pthread_cond_wait(&input_buffer_is_clear, &input_buffer_mutex); 
+
+        // Release the lock
+        pthread_mutex_unlock(&input_buffer_mutex);
+    }
+
+    return NULL;
 }
 
-void separate_lines(char* input_buffer, char* output_buffer){
+
+/**********************************************************************
+ *                          *
+ **********************************************************************/
+
+void* separate_lines(void* arg){
+    char raw_tmp_buffer[MAX_LINE_SIZE];
+    char processed_tmp_buffer[MAX_LINE_SIZE];
+
+    while(1){
+    memset(raw_tmp_buffer, '\0', MAX_LINE_SIZE);
+    memset(processed_tmp_buffer, '\0', MAX_LINE_SIZE);
+    // Lock buf 1
+    pthread_mutex_lock(&input_buffer_mutex); 
+
+    // Wait for input_buf to have data
+    pthread_cond_wait(&input_buffer_has_data, &input_buffer_mutex); 
+
+    // We now have the lock
+    // Copy data to temp buffer
+    strcpy(raw_tmp_buffer, input_buffer);
+    
+    // Signal the producer that the buffer is clear
+    pthread_cond_signal(&input_buffer_is_clear);
+
+    // Unlock the buffer for the producer
+    pthread_mutex_unlock(&input_buffer_mutex);
+
+    printf("SEP LINES GOT: %s", raw_tmp_buffer);
+    // Process copied data
+    //replace_newlines(raw_tmp_buffer)
+    // Lock buf 2 -> will wait and make sure lock is available. RP thread should be ready generally
+    // copy proccessed into buf 2
+    // Notify RP thread that buf 2 has data 
+    // unlock
+    }
+}
+
+
+void replace_newlines(char* input_buffer, char* output_buffer){
 
     for (int i=0; i <  MAX_LINE_SIZE; i++) {
         if (input_buffer[i] == '\n'){
@@ -22,8 +79,16 @@ void separate_lines(char* input_buffer, char* output_buffer){
         //printf("character being process: %d\n", input_buffer[i]);
         //printf("character being outputted: %d\n", output_buffer[i]);
     }
+   
 }
 
+
+
+
+
+/**********************************************************************
+ *                          *
+ **********************************************************************/
 void replace_plusses(char* input_buffer, char* output_buffer){
     int input_index = 0;
     int output_index = 0;
@@ -39,4 +104,8 @@ void replace_plusses(char* input_buffer, char* output_buffer){
             output_index += 1;
         }
     }
+}
+
+void output_lines(char* output_buffer){
+    // as soon as the 
 }
--- Assignment4/main.c
+++ Assignment4/main.c
@@ -3,24 +3,24 @@
 int main () {
     bool stop_command_recieved = false;
 
-    //Todo: alter size
-    char input_buffer[MAX_LINE_SIZE];        // get_input_lines > input_buffer > separate_lines
-    char separated_buffer[MAX_LINE_SIZE];    // separate_lines > separated_buffer > replace_plusses
-    char output_buffer[MAX_LINE_SIZE];       // replace_plusses > output_buffer > replace_plusses
+    
 
-
+    pthread_t p, c;
     int lines_processed = 0;
     while(!stop_command_recieved && lines_processed < MAX_NUMBER_LINES) 
     {
         memset(input_buffer, '\0', MAX_LINE_SIZE);
-        get_input_lines(input_buffer);
-        printf("input gotten: %s\n", input_buffer);
-        separate_lines(input_buffer, separated_buffer);
-        printf("lines separated: %s\n", separated_buffer);
-        replace_plusses(separated_buffer, output_buffer);
-        printf("plusses replaced: %s\n", output_buffer);
-        // output_lines
 
+        pthread_create(&p, NULL, get_input_lines, NULL);
+
+        pthread_create(&c, NULL, separate_lines, NULL);
+
+        //printf("lines separated: %s\n", separated_buffer);
+        //replace_plusses(separated_buffer, output_buffer);
+        //printf("plusses replaced: %s\n", output_buffer);
+        //output_lines(output_buffer);
+        pthread_join(p, NULL);
+        pthread_join(c, NULL);
         lines_processed++;
     }
     return 0;
