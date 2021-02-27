340bd3cd4a170dbb70dca0084b0729962ac3336e
Orion Junkins <junkinso@oregonstate.edu>
2021-02-26 20:04:01 -0800

    Multi client server working with fork()

--- Assignment5/enc_server.c
+++ Assignment5/enc_server.c
@@ -59,38 +59,71 @@ int main(int argc, char *argv[]){
   listen(listenSocket, 5); 
   
   // Accept a connection, blocking if one is not available until one connects
-  while(1){
-    // Accept the connection request which creates a connection socket
-    connectionSocket = accept(listenSocket, 
+    while(1){
+        printf("Con Soc: %d\n", connectionSocket);
+        // Accept the connection request which creates a connection socket
+        connectionSocket = accept(listenSocket, 
                 (struct sockaddr *)&clientAddress, 
                 &sizeOfClientInfo); 
-    if (connectionSocket < 0){
-      error("ERROR on accept");
-    }
+        if (connectionSocket < 0){
+            error("ERROR on accept");
+        }
 
-    printf("SERVER: Connected to client running at host %d port %d\n", 
-                          ntohs(clientAddress.sin_addr.s_addr),
-                          ntohs(clientAddress.sin_port));
+        // Fork a new process
+        pid_t spawnPid = fork();
+        switch(spawnPid){
+            case -1:
+                perror("fork() failure\n");
+                exit(1);
+                break;
+            case 0:
+                // In the child
+                close(listenSocket);
+                printf("SERVER: Connected to client running at host %d port %d\n", 
+                            ntohs(clientAddress.sin_addr.s_addr),
+                            ntohs(clientAddress.sin_port));
 
-    // Get the message from the client and display it
-    memset(buffer, '\0', 256);
-    // Read the client's message from the socket
-    charsRead = recv(connectionSocket, buffer, 255, 0); 
-    if (charsRead < 0){
-      error("ERROR reading from socket");
-    }
-    printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+                // Get the message from the client and display it
+                memset(buffer, '\0', 256);
+                // Read the client's message from the socket
+                charsRead = recv(connectionSocket, buffer, 255, 0); 
+                if (charsRead < 0){
+                error("ERROR reading from socket");
+                }
+                printf("SERVER: I received this from the client: \"%s\"\n", buffer);
 
-    // Send a Success message back to the client
-    charsRead = send(connectionSocket, 
-                    "I am the server, and I got your message", 39, 0); 
-    if (charsRead < 0){
-      error("ERROR writing to socket");
+                // Send a Success message back to the client
+                charsRead = send(connectionSocket, 
+                                "I am the server, and I got your message", 39, 0); 
+                if (charsRead < 0){
+                error("ERROR writing to socket");
+                }
+                // Close the connection socket for this client
+                close(connectionSocket); 
+                exit(0);
+            default:
+                // In the parent
+                close(connectionSocket);
+        }
+
+    //Fork
+    //In parent:
+        // Close connectionSocket
+    //In child :
+        // Close listen socket
+        // Recieve validation & size
+        // If invalid:
+            // Exit with Error
+            // Terminate Process
+            //otherwise,
+        // Send confirmation to client
+        // Recieve data
+        // encrypt
+        // Send encrypted
+        // Close
+    //int wait_result = waitpid(node_ptr->PID, &wstatus, WNOHANG); Close BG processes
     }
-    // Close the connection socket for this client
-    close(connectionSocket); 
-  }
-  // Close the listening socket
-  close(listenSocket); 
-  return 0;
+    // Close the listening socket
+    close(listenSocket); 
+    return 0;
 }
